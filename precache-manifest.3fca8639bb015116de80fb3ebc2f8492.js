1274b9fbc341932be6f08983a30509ffd57dc55f
Aprajita Murari <aprajitamurari@Aprajitas-MacBook-Air.local>
2021-11-21 18:18:23 +0530

    Updates

---
+++ precache-manifest.3fca8639bb015116de80fb3ebc2f8492.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c1bd3089dd7b8270c4a902bdadb66b",
+    "url": "/Learn-React-Project/index.html"
+  },
+  {
+    "revision": "44145c278338286d9370",
+    "url": "/Learn-React-Project/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "3f8522c37efd5c30e767",
+    "url": "/Learn-React-Project/static/css/main.b60f863f.chunk.css"
+  },
+  {
+    "revision": "44145c278338286d9370",
+    "url": "/Learn-React-Project/static/js/2.e143eb6e.chunk.js"
+  },
+  {
+    "revision": "77e1b13cea108a173725d7538d8fb564",
+    "url": "/Learn-React-Project/static/js/2.e143eb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014351961494c8acc9c0",
+    "url": "/Learn-React-Project/static/js/3.505fa462.chunk.js"
+  },
+  {
+    "revision": "3f8522c37efd5c30e767",
+    "url": "/Learn-React-Project/static/js/main.67c7db2c.chunk.js"
+  },
+  {
+    "revision": "8c02bda66db1f8e860c7",
+    "url": "/Learn-React-Project/static/js/runtime-main.fa98ec28.js"
+  }
+]);
